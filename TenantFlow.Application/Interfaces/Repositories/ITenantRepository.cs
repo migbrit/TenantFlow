0a0d0d04d1c9db3f783f6976688f9ea948169a00
Miguel Brito <miguelfrbrito@gmail.com>
2026-03-22 18:01:08 -0300

    add ITenantRepository in application layer

---
+++ TenantFlow.Application/Interfaces/Repositories/ITenantRepository.cs
@@ -0,0 +1,10 @@
+﻿using TenantFlow.Domain.Entities;
+
+namespace TenantFlow.Application.Interfaces.Repositories;
+
+public interface ITenantRepository
+{
+    Task<Tenant?> GetByIdAsync(Guid id);
+    Task<bool> ExistsByEmailAsync(string email);
+    Task AddAsync(Tenant tenant);
+}
