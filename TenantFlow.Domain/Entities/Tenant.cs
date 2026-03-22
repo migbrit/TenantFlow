bebb1d02b8020f60dd2fbb426eb8c5c399291e65
Miguel Brito <miguelfrbrito@gmail.com>
2026-03-22 18:00:42 -0300

    update Tenant domain entity

--- TenantFlow.Domain/Entities/Tenant.cs
+++ TenantFlow.Domain/Entities/Tenant.cs
@@ -5,13 +5,32 @@ namespace TenantFlow.Domain.Entities;
 
 public class Tenant : AuditableEntity
 {
-    public string Name { get; private set; }
+    public string Name { get; private set; } = null!;
 
-    public string Email { get; private set; }
+    public string Email { get; private set; } = null!;
 
     public bool IsActive { get; private set; }
 
-    private Tenant() { }
+    private Tenant() { }  // EF Core
+
+    public void Deactivate() => IsActive = false;
+    public void Activate() => IsActive = true;
+
+    public static Tenant Create (string name, string email)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException("Name is required.");
+
+        if (string.IsNullOrWhiteSpace(email))
+            throw new DomainException("Email is required.");
+
+        return new Tenant
+        {
+            Name = name,
+            Email = email,
+            IsActive = true
+        };
+    }
 
     public void Rename(string name)
     {
@@ -19,6 +38,7 @@ public class Tenant : AuditableEntity
             throw new DomainException("Name cannot be empty.");
 
         Name = name;
+        SetUpdatedAt();
     }
 
     public void ChangeEmail(string email)
@@ -27,5 +47,6 @@ public class Tenant : AuditableEntity
             throw new DomainException("Email cannot be empty.");
 
         Email = email;
+        SetUpdatedAt();
     }
 }
